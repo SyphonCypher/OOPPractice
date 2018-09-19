b051c0957a1c63df022743f4c700aaadd39acf45
SyphonCypher <tim_horta@outlook.com>
2018-09-19 13:20:00 -0400

    initial commit

---
+++ OOPPractice/Dog.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Dog
+    {
+        //Fields 
+        private int hairLength;
+        private double height;
+        private double runningSpeed;
+        private double weight;
+        private string name;
+
+        //Properties
+        public int HairLength
+        {
+            get { return this.hairLength; }
+            set { this.hairLength = value; }
+        }
+
+        public double Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public double RunningSpeed
+        {
+            get { return this.runningSpeed; }
+            set { this.runningSpeed = value; }
+        }
+
+        public double Weight
+        {
+            get { return this.weight; }
+            set { this.weight = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public Dog()
+        {
+            //Default constructor
+        }
+
+        public Dog(int hair, double tall, double speed, double thiccness, string tag)
+        {
+            this.HairLength = hair;
+            this.Height = tall;
+            this.RunningSpeed = speed;
+            this.Weight = thiccness;
+            this.Name = tag;
+        }
+
+        //Methods
+        public double GetSpeed(int distance)
+        {
+            RunningSpeed = RunningSpeed * distance;
+            GetDetails();
+            return RunningSpeed;
+        }
+
+        public void GetDetails()
+        {
+            Console.WriteLine("Your pet's name is {0}. It has {1} inches of hair.", Name, HairLength);
+            Console.WriteLine("{0} is {1} inches tall and weighs {2} pounds.", Name, Height, Weight);
+            Console.WriteLine("{0} can run {1}mph.", Name, RunningSpeed);
+        }
+    }
+}
---
+++ OOPPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dog woofy = new Dog();
+            //Dog spot = new Dog();
+            //Dog woofy = new Dog(7, 8.5, 33.2, 190.1, "Woofy");
+            //Dog spot = new Dog(15, 10.5, 100.9, 250.3, "Spot");
+            //Console.WriteLine("Woofy's hair length is: {0}", woofy.HairLength);
+            //Console.WriteLine("Spot's hair length is: {0}", spot.HairLength);
+
+            //woofy.HairLength = 4;
+            //spot.HairLength = 6;
+            //Console.WriteLine("Woofy's hair length is: {0}", woofy.HairLength);
+            //Console.WriteLine("Spot's hair length is: {0}", spot.HairLength);
+
+            //Console.WriteLine("Woofy is running: {0}", woofy.GetSpeed(7)); 
+        }
+    }
+}
